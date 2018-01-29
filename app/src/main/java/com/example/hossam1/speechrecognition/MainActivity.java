16c3b1afd012aa5e73e440a77ff6f2c36bc5568b
Hossammaher <hossammaher34@gmail.com>
2018-01-29 13:47:54 +0200

    first commit

---
+++ app/src/main/java/com/example/hossam1/speechrecognition/MainActivity.java
@@ -0,0 +1,238 @@
+package com.example.hossam1.speechrecognition;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+    Intent mSpeechRecognizerIntent;
+    EditText input;
+    ImageView click_to_speak;
+//    TextView show;
+    ListView listview ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        input = (EditText) findViewById(R.id.editText);
+//        show = (TextView) findViewById(R.id.show);
+        listview=(ListView)findViewById(R.id.list);
+
+        click_to_speak = (ImageView) findViewById(R.id.imageView);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+
+
+        click_to_speak.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                switch (motionEvent.getAction()) {
+
+                    case MotionEvent.ACTION_UP:
+                        input.setHint("input show here");
+                        mSpeechRecognizer.stopListening();
+
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        checkPermission();
+                        input.setText("");
+                        input.setHint("listening ");
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+
+                ArrayList<String> matches2 = matches.getString(matches.get(0));
+
+
+
+
+
+                if (matches != null) {
+
+                    input.setText(matches.get(0));
+//                    show.append(matches.get(0) + "\n");
+
+//
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                            android.R.layout.simple_list_item_1,matches2);
+
+                    listview.setAdapter(arrayAdapter);
+
+
+                }
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+    }
+
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO) !=  PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},1);
+
+        } else {
+
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        if (requestCode == 1)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+
+//        private void checkPermission() {
+//            if (ContextCompat.checkSelfPermission(MainActivity.this,
+//                    Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+//
+//            } else {
+//                requestAudioPermission();
+//            }
+//
+//    }
+//
+//    private void requestAudioPermission() {
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                Manifest.permission.RECORD_AUDIO)) {
+//
+//            new AlertDialog.Builder(this)
+//                    .setTitle("Permission needed")
+//                    .setMessage("This permission is needed because of this and that")
+//                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                        }
+//                    })
+//
+//            .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.dismiss();
+//                }
+//            }).create().show();
+//
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[] {Manifest.permission.RECORD_AUDIO}, 1);
+//        }
+//    }
+//
+//    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+//        if (requestCode == 1)  {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+//
+//    private void checkPermission() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+//                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+//                        Uri.parse("package:" + getPackageName()));
+//                startActivity(intent);
+//                finish();
+//            }
+//        }
+//
+//    }
+
+}
